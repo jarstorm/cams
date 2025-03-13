f557e6b401561623b0a009cd6df2f0c3bec79ed3
Javier Abad <javier.abad@emurgo.io>
2025-03-13 16:51:23 +0100

    Setup deno

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import netlify from '@astrojs/netlify';
+import deno from '@deno/astro-adapter';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-
+  adapter: deno(),
   vite: {
     plugins: [tailwindcss()]
   },
