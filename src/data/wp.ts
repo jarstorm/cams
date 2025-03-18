f4cdce3386a219e49f5316056369027a43cd3406
Javier Abad <javier.abad@emurgo.io>
2025-03-18 10:17:09 +0100

    Hero sample

--- src/data/wp.ts
+++ src/data/wp.ts
@@ -1,5 +1,5 @@
-//const BASE_URL = "https://www.hispacams.com/wp-json/wp/v2";
-const BASE_URL = "https://www.nasa.gov/wp-json/wp/v2";
+const BASE_URL = "https://www.hispacams.com/wp-json/wp/v2";
+//const BASE_URL = "https://www.nasa.gov/wp-json/wp/v2";
 
 export const getPages = async () => {
     try {
@@ -21,3 +21,28 @@ export const getPosts = async () => {
         return [];
     }    
 }
+
+
+export const getPostData = async (slug: string) => {
+    try {
+        const response = await fetch(`${BASE_URL}/posts/?slug=${slug}`);
+        return await response.json()    
+    } catch (e) {
+        console.log(e);
+        return [];
+    }    
+}
+
+export const getWebcamData = async (slug: string) => {
+    try {
+        const response = await fetch(`${BASE_URL}/pages?per_page=100&page=1`);
+        return await response.json()    
+    } catch (e) {
+        console.log(e);
+        return [];
+    }    
+}
+
+// En /tags hay cosas
+// En /comments
+
