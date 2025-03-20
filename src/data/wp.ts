3f10e601961cf8f2b586939948bc8084f5d6f7b1
Javier Abad <javier.abad@emurgo.io>
2025-03-20 12:04:42 +0100

    Update landing

--- src/data/wp.ts
+++ src/data/wp.ts
@@ -2,47 +2,63 @@ const BASE_URL = "https://www.hispacams.com/wp-json/wp/v2";
 //const BASE_URL = "https://www.nasa.gov/wp-json/wp/v2";
 
 export const getPages = async () => {
-    try {
-        const response = await fetch(`${BASE_URL}/pages`);
-        return await response.json()    
-    } catch (e) {
-        console.log(e);
-        return [];
-    }
-}
-
+  try {
+    const response = await fetch(`${BASE_URL}/pages`);
+    return await response.json();
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
+};
 
 export const getPosts = async () => {
-    try {
-        const response = await fetch(`${BASE_URL}/posts?_fields=date,slug,link,title,featured_media,_links,_embedded&_embed`);
-        return await response.json()    
-    } catch (e) {
-        console.log(e);
-        return [];
-    }    
-}
-
+  try {
+    const response = await fetch(
+      `${BASE_URL}/posts?_fields=date,slug,link,title,featured_media,_links,_embedded&_embed`,
+    );
+    return await response.json();
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
+};
 
 export const getPostData = async (slug: string) => {
-    try {
-        const response = await fetch(`${BASE_URL}/posts/?slug=${slug}`);
-        return await response.json()    
-    } catch (e) {
-        console.log(e);
-        return [];
-    }    
-}
+  try {
+    const response = await fetch(`${BASE_URL}/posts/?slug=${slug}`);
+    return await response.json();
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
+};
 
 export const getWebcamData = async (slug: string) => {
-    try {
-        const response = await fetch(`${BASE_URL}/pages?per_page=100&page=1`);
-        return await response.json()    
-    } catch (e) {
-        console.log(e);
-        return [];
-    }    
-}
+  try {
+    const response = await fetch(`${BASE_URL}/pages?per_page=100&page=1`);
+    return await response.json();
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
+};
 
-// En /tags hay cosas
-// En /comments
-
+export const getFeaturedCams = async () => {
+  return [
+    {
+      img: "https://www.hispacams.com/get_imagen_ws.php?id=243&size=s",
+      name: "Estación invernal Valgrande – Pajares: Panorámica Cuitunigru – Las Ubiñas",
+      slug: "estacion-invernal-valgrande-pajares-panoramica-cuitunigru-las-ubinas",
+    },
+    {
+      img: "https://www.hispacams.com/get_imagen_ws.php?id=256&size=s",
+      name: "Valdelugueros – Lugueros",
+      slug: "estacion-invernal-valgrande-pajares-panoramica-cuitunigru-las-ubinas",
+    },
+    {
+      img: "https://www.hispacams.com/get_imagen_ws.php?id=243&size=s",
+      name: "Estación invernal Valgrande – Pajares: Panorámica Cuitunigru – Las Ubiñas",
+      slug: "estacion-invernal-valgrande-pajares-panoramica-cuitunigru-las-ubinas",
+    },
+  ];
+};
